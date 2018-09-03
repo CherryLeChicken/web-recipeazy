82324dcf46806cd48f310cd275d66d5db2ae42d7
theultra <cherryke07@gmail.com>
2018-09-03 16:17:43 +0000

    Attempting to make navbar responsive with Semantic UI

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -3,10 +3,15 @@ class CommentsController < ApplicationController
   
   def create
     @post = Post.find(params[:post_id])
+    
     @comment = Comment.create(params[:comment].permit(:content))
+    #IF YOU SEE THIS PAGE, IT MEANS YOU
+    #DID NOT ENTER ANY CONTENT IN THE COMMENTS FORM.
+    #PLEASE GO BACK TO CONTINUE TO RECIPEAZY. THANK YOU.
+    
     @comment.user_id = current_user.id
     @comment.post_id = @post.id
-    
+
     if @comment.save
       redirect_to post_path(@post)
     else
--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,4 +1,5 @@
 class Comment < ActiveRecord::Base
+  devise :validatable
   belongs_to :post
   belongs_to :user
 end
